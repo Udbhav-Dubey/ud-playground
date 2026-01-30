5d84a71930133aa285031e31ac0769ac4878ed94
Udbhav-Dubey <udbhavdubey44@gmail.com>
2026-01-30 11:41:30 +0530

    took a a shot at allignment previous one was horribely wrong it was tracking asked bytes instead of curr pos which it should monitor now i allign everysingle time to get to best place

--- custom_memory_allocator/bump/bumpy_alloc.cpp
+++ custom_memory_allocator/bump/bumpy_alloc.cpp
@@ -17,27 +17,34 @@ class Bumpy{
             beg=arr;
         }
        char* give_me_bytes(int N){
-            if (curr+N>=end){
-                std::cout << "not possible\n";
+            // first check is it even possible 
+            // first check aayega allignement ka 
+            // agar allign hai to theek nahi to allign karo 
+            // allign fir oom checking ki ab possible or not 
+            // if possible plus n karo aur return old one okay
+            // allignement is a destination there we need to reach before allocating size 
+            // okay 
+            if (curr+N>end){
+                std::cout << "cannot allocate this much memory as it would lead to out of memory\n";
                 flag=0;
                 return nullptr;
             }
-            int alli=alignof(std::max_align_t);
-            char* old_curr=curr;
-            if (N%alli==0){
-                curr+=N;
-            } 
-            else {
-                int x=N%alli;
-                 N+=(alli-x);
-                curr+=N;
-            }
-            return old_curr;
-        }
+            char *oldcurr{nullptr};
+            uintptr_t mask=alignof(std::max_align_t)-1;
+            // i am gonna allign never the less even if its already alligned 
+            uintptr_t addr=reinterpret_cast<uintptr_t>(curr);
+            addr+=mask;
+            addr&=~(mask);
+            curr=reinterpret_cast<char*>(addr);
+            if (curr>end){std::cout << "cannot allocate this much memory allign issue\n";flag=0;return nullptr;}
+            oldcurr=curr;
+            curr+=N;
+            return oldcurr;
+       }
         void debug(){
-            std:: cout << "end : " << (int*)end <<"\n";
-            std:: cout << "curr : " << (int*)curr <<"\n";
-            std:: cout << "arr is : " << (int*)arr <<"\n" ;
+            std:: cout << "end : " << (void*)end <<"\n";
+            std:: cout << "curr : " << (void*)curr <<"\n";
+            std:: cout << "arr is : " << (void*)arr <<"\n" ;
         }
         void reset(){
             curr=beg;
